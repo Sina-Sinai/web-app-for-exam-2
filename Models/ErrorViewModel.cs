4f0afd38284f0d4ab372068ab61b80ffa4141ad1
Sina <alisinai850@gmail.com>
2023-02-21 18:22:01 +0330

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace web_app_for_exam_2.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
